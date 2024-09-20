9e24d6cb80e87e3147b2769808db01fd180346e9
ajcanepa <ajcanepa@ubu.es>
2024-09-20 09:27:55 +0000

    clase 20 sept 2024

--- Objetos_Teoria.R
+++ Objetos_Teoria.R
@@ -44,3 +44,76 @@ DF2$Name
 
 DF$Year[DF$Name == "Pedro"]
 
+Lista <- list(
+  Vec_Num = x,
+  Vec_Char = as.character(y),
+  Mat = Matrix,
+  DataFrame = DF
+)
+
+Lista$DataFrame$Name[3]
+
+
+data(mtcars)
+mtcars
+
+RegLi <- lm(mpg ~ disp + cyl + wt, data = mtcars)
+summary(RegLi)
+
+
+
+x <- c(1,2,3)
+x2 <- c(1,2,3, 4)
+
+y <- sin(x)
+
+mean(x, na.rm = TRUE)
+
+
+x2 + y
+
+
+presion_arterial <- 100
+
+if (presion_arterial > 140) {
+  categoria <- "Alta"
+} else {
+  categoria <- "Normal"
+}
+
+print(categoria)
+
+presion_sistolica <- c(120, 130, 110, 140, 135, 150)
+edad_grupo <- c("Joven", "Adulto", "Joven", "Adulto", "Adulto", "Adulto")
+
+media_presion <- tapply(presion_sistolica, edad_grupo, mean)
+
+print(media_presion)
+
+
+presion <- 280
+objetivo <- 120
+dias <- 0
+
+while (presion > objetivo) {
+  presion <- presion - 5
+  dias <- dias + 1
+}
+
+dias
+print(dias) 
+
+
+alturas <- c(1.70, 1.75, 1.60, 1.75)
+pesos <- c(65, 75, 50, 83)
+imc <- numeric(length(alturas))
+
+for (i in 1:3) {
+  imc[i] <- pesos[i] / alturas[i]^2
+}
+
+for (i in 1:length(alturas)) {
+  imc[i] <- pesos[i] / alturas[i]^2
+}
+
+print(imc)
